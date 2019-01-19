7ef4c1aee0e500bb7903e6f58d1f3fd20c94dbd1
Daisuke Murase <typester@cpan.org>
2019-01-19 12:36:39 +0900

    layout for RBK-2000BT

---
+++ app/src/main/java/org/unknownplace/keyboardlayouts/InputDeviceReceiver.java
@@ -0,0 +1,11 @@
+package org.unknownplace.keyboardlayouts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
